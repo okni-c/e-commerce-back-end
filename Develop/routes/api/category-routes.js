225a68dba39d19be24ff5927cc387a21634d1f0d
okni-c <oknistay@gmail.com>
2021-06-26 15:14:37 -0500

    Finished crud on category route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -46,10 +46,38 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(() => { res.json({ message: 'New category created'}) })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'Cannot update - No category found with this id' });
+        return;
+      }
+      res.json({ message: 'Category updated' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
